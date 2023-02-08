2a69d09d33e6494f9d3c3433c05f521bb19d8dfd
DESKTOP-NPQULOB\JuanPresiga\D:\Mis Documentos <jcpresiga>
2023-02-07 21:41:05 -0500

    implementacion login con error de navegacion

---
+++ src/layouts/login/LayoutBannerLogin.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { View, Image, StyleSheet, Text, ImageBackground } from "react-native";
+
+const LayoutBannerLogin = () => {
+  return (
+    <View>
+
+    <ImageBackground style={{width: '100%', height: 280}}
+        source={require("../../../assets/images/login_bg.png")}>
+            <View style={styles.textView}>
+                <Image
+                    source={require("../../../assets/images/logo.png")}
+                    style={{width: '40%', height: 50}}
+                ></Image>
+                <Text style={styles.imageText}>Welcome to your sales assistant</Text>
+            </View>
+    </ImageBackground>
+    </View>
+  );
+};
+
+export default LayoutBannerLogin;
+
+const styles = StyleSheet.create({
+    logo: {
+        height: 410,
+        width: 410,
+    },
+    textView: {
+        position: 'absolute',
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        
+      },
+      imageText: {
+        fontSize: 30,
+        color: '#1592b9',
+        fontWeight: 'bold',
+        textAlign: 'center',
+      },
+})
