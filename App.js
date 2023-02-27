48f9fb74f9571faf6d1698496255332e4a73b199
gedar1971 <gedar1971@gmail.com>
2023-02-26 19:28:51 -0500

    refactor layoutWellcome

--- App.js
+++ App.js
@@ -2,12 +2,17 @@ import { StyleSheet, View } from 'react-native';
 import ItemState  from './src/contexts/itemContext/ItemState'
 import Main from './src/Main';
 //import LayoutTemplate from './src/layouts/LayoutTemplate';
+//import LayoutWellcome from './src/layouts/wellcome/LayoutWellcome';
+import LayoutCharts from './src/layouts/chartsData/LayoutCharts';
+//import LayoutTemplate from './src/layouts/LayoutTemplate';
 
 export default function App() {
   return (
     <ItemState>
-      <Main/>
+      {/* <Main/> */}
       {/* <LayoutTemplate/> */}
+      <LayoutCharts/>
+      {/* <LayoutWellcome/> */}
     </ItemState>
    
   );
